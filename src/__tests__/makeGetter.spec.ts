96de537ecf3a90d0c45d44a864edd696ff02817c
Kevin Groat <apps@kevingroat.io>
2018-01-06 10:45:46 -0500

    Added tests

---
+++ src/__tests__/makeGetter.spec.ts
@@ -0,0 +1,16 @@
+
+import { getter } from '../makeGetter'
+
+describe('getter', () => {
+  it('should return a getter whose `get` function is the one passed in', () => {
+    const expected = jest.fn()
+    const actual = getter(expected)
+    expect(actual.get).toBe(expected)
+  })
+
+  it('should return a getter whose `defaultVal` is the one passed in', () => {
+    const expected = {}
+    const actual = getter(jest.fn(), expected)
+    expect(actual.defaultVal).toBe(expected)
+  })
+})
