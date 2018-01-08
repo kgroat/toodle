b27892b250e939e569b72afd86d17e5a64bc7fc3
Kevin Groat <apps@kevingroat.io>
2018-01-08 12:13:52 -0500

    added way of creating async mutators

--- src/makeMutator.ts
+++ src/makeMutator.ts
@@ -1,10 +1,19 @@
 
 import {
+  AsyncMutator,
+  ReaderMutator,
   Mutator,
   DeepReadonly,
 } from './types'
 
 export function mutator<S, O> (mutate: (state: DeepReadonly<S>, options: O) => DeepReadonly<S>, defaultOptions?: O): Mutator<S, O> {
+  return {
+    mutate,
+    defaultOptions,
+  }
+}
+
+export function asyncMutator<S, O, M extends ReaderMutator<S, any>> (mutate: (mutators: M, options: O) => void, defaultOptions?: O): AsyncMutator<S, O, M> {
   return {
     mutate,
     defaultOptions,
